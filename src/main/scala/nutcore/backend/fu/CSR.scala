6f0a26cd3849674633d840789dafc773b0b679cf
ByeBeihai <2637039533@qq.com>
2022-09-28 13:43:19 +0000

    clint base address set to 0x02000000,now rtthread of uartlite io work well on u250 fpga

--- src/main/scala/nutcore/backend/fu/CSR.scala
+++ src/main/scala/nutcore/backend/fu/CSR.scala
@@ -564,7 +564,7 @@ class CSR(implicit val p: NutCoreConfig) extends NutCoreModule with HasCSRConst{
   BoringUtils.addSink(mtip, "mtip")
   BoringUtils.addSink(meip, "meip")
   BoringUtils.addSink(msip, "msip")
-  //mipWire.t.m := mtip
+  mipWire.t.m := mtip
   mipWire.e.m := meip
   mipWire.s.m := msip
   
