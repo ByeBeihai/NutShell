4600986ae503a5a1b8157046d494105a303380f7
ByeBeihai <2637039533@qq.com>
2022-10-13 13:10:01 +0000

    last version can run on fpga,now delete useless signal : icantwrite

--- src/main/scala/nutcore/backend/seq/SIMD_EXU.scala
+++ src/main/scala/nutcore/backend/seq/SIMD_EXU.scala
@@ -362,7 +362,7 @@ class new_SIMD_EXU(implicit val p: NutCoreConfig) extends NutCoreModule {
 
   for(i <- 0 to FuType.num-1){
     io.forward(i).valid := io.out(i).valid
-    io.forward(i).wb.rfWen := io.out(i).bits.decode.ctrl.rfWen && !IcantWrite(i)
+    io.forward(i).wb.rfWen := io.out(i).bits.decode.ctrl.rfWen //&& !IcantWrite(i)
     io.forward(i).wb.rfDest := io.out(i).bits.decode.ctrl.rfDest
     io.forward(i).wb.rfData := MuxLookup(fuType(i),aluOut,Array(FuType.alu->aluOut,FuType.alu1->alu1Out,FuType.lsu->lsuOut,FuType.csr->csrOut,FuType.mdu->mduOut,FuType.simdu->simduOut,FuType.simdu1->simdu1Out))
     io.forward(i).fuType := io.out(i).bits.decode.ctrl.fuType
