81ca9e650a9ddfd02fc77070f9c6984ad599cb62
ByeBeihai <2637039533@qq.com>
2025-03-19 07:02:52 +0000

    prepare to split the forward path to int and float

--- src/main/scala/nutcore/backend/fu/fpu/FMA.scala
+++ src/main/scala/nutcore/backend/fu/fpu/FMA.scala
@@ -5,7 +5,6 @@ import chisel3.util._
 import nutcore.backend.fu.fpu.fudian._
 import float._
 import chisel3.util.experimental.BoringUtils
-
 trait FMAOpType {
   def fadd_s = "b100_0010".U
   def fsub_s = "b100_0011".U
@@ -36,7 +35,7 @@ class FMA_CtrlSignal extends Bundle{
   val isSub = Input(Bool())
   val isInv = Input(Bool())
 }
-class FMA_impl(ftype: FType)(implicit val p:NutCoreConfig)extends Module {
+class FMA_impl(ftype: FType)extends Module {
   val io = IO(new Bundle() {
     val flush = Input(Bool())
     val in = Flipped(Decoupled(new FPDataIn(ftype.expWidth, ftype.sigWidth, 3)))
@@ -52,59 +51,39 @@ class FMA_impl(ftype: FType)(implicit val p:NutCoreConfig)extends Module {
   val isInv = io.ctrl.isInv
 
   val fmulInValid = useMul && io.in.valid
-  val fmulResult = Wire(new FMULOut(ftype.expWidth, ftype.sigWidth))
   val fmulOutValid, fmulInReady, faddOutValid, faddInReady = Wire(Bool())
-  val fmul = FMUL(a, b, rm, ftype.expWidth, ftype.sigWidth, fmulInValid, faddInReady)
-  fmulResult := fmul._1
+  val fmul = FMUL(a, b, rm, ftype.expWidth, ftype.sigWidth, fmulInValid, faddInReady, io.flush || io.out.fire)
+  val fmulResult = fmul._1
   fmulOutValid := fmul._2
   fmulInReady := fmul._3
 
-  val handshaked = fmulOutValid && faddInReady && useAdd
-  val mul2add_valid = RegInit(false.B)
-  
-  when(handshaked) {
-    mul2add_valid := true.B
-  }
-  when(faddOutValid && io.out.ready) {
-    mul2add_valid := false.B
-  }
-  when(io.flush){
-    mul2add_valid := false.B
-  }
+  //**********************************************************************//
+  val pipeline = Module(new PipelineReg(new FMULToFADD(ftype.expWidth, ftype.sigWidth)))
+  pipeline.io.in.bits.fp_prod := fmulResult.tofadd.fp_prod
+  pipeline.io.in.bits.inter_flags := fmulResult.tofadd.inter_flags
+  pipeline.io.flush := io.flush
+  pipeline.io.in.valid := fmulOutValid && useAdd
+  pipeline.io.out.ready := faddInReady
+  val mul2add_valid = pipeline.io.out.valid
+  val fp_prod = pipeline.io.out.bits.fp_prod
+  val inter_fflags = pipeline.io.out.bits.inter_flags
+  //**********************************************************************//
 
-  val faddInValid = RegNext(!useMul && io.in.valid) && io.in.valid || mul2add_valid
-  val a_inter_fflags = RegEnable(fmulResult.tofadd.inter_flags, handshaked)
-
+  val faddInValid = !useMul && io.in.valid || mul2add_valid
   val mul2add_width = ftype.expWidth + 2 * ftype.sigWidth
   def padd_tail(x: UInt, w: Int): UInt = Cat(x, 0.U((w - x.getWidth).W))
   val faddIn = Wire(Vec(2, UInt(mul2add_width.W)))
-  faddIn(0) := Mux(useMul, RegEnable(fmulResult.tofadd.fp_prod, handshaked), RegNext(padd_tail(a, mul2add_width)))
-  faddIn(1) := RegNext(padd_tail(Mux(useMul, c, b), mul2add_width))
+  faddIn(0) := Mux(useMul, fp_prod, padd_tail(a, mul2add_width))
+  faddIn(1) := padd_tail(Mux(useMul, c, b), mul2add_width)
   val fadd = FADD(faddIn(0), faddIn(1), rm, isSub, isInv, ftype.expWidth, 2 * ftype.sigWidth, ftype.sigWidth,
-    faddInValid, io.out.ready, mul2add_valid, false.B, Some(a_inter_fflags))
+    faddInValid, io.out.ready, io.flush || io.out.fire, mul2add_valid, false.B, Some(inter_fflags))
   faddOutValid := fadd._3
   faddInReady := fadd._4
 
-  io.in.ready := DontCare
-  io.out.valid := Mux(useAdd, fadd._3, fmulOutValid)
+  io.in.ready := faddInReady & fmulInReady
+  io.out.valid := Mux(useAdd, faddOutValid, fmulOutValid)
   io.out.bits.result := Mux(useAdd, fadd._1, fmulResult.result)
   io.out.bits.fflags := Mux(useAdd, fadd._2, fmulResult.fflags)
-
-  if(p.FPGAPlatform){
-    if(ftype == fp32){
-      //BoringUtils.addSource(io.out.valid,"ilafp32outvalid")
-    }else{
-      //BoringUtils.addSource(io.out.valid,"ilafp64outvalid")
-      //BoringUtils.addSource(io.in.valid, "ilafp64invalid")
-      //BoringUtils.addSource(mul2add_valid, "ilafp64mul2add_valid")
-      //BoringUtils.addSource(handshaked, "ilafp64handshaked")
-      //BoringUtils.addSource(useAdd, "ilafp64useadd")
-      //BoringUtils.addSource(useMul, "ilafp64usemul")
-      //BoringUtils.addSource(faddOutValid, "ilafp64faddOutValid")
-      //BoringUtils.addSource(fmulOutValid, "ilafp64fmulOutValid")
-      //BoringUtils.addSource(faddInReady, "ilafp64faddInReady")
-    }
-  }
 }
 
 class FMAIO(ftype: FType) extends Bundle {
@@ -114,7 +93,7 @@ class FMAIO(ftype: FType) extends Bundle {
   val out = DecoupledIO(new FPDataOut(ftype.expWidth, ftype.sigWidth))
 }
 
-class FMA(implicit val p:NutCoreConfig) extends Module with FMAOpType {
+class FMA extends Module with FMAOpType {
   val S = fp32
   val D = fp64
   val io = IO(new FMAIO(D))
@@ -141,44 +120,42 @@ class FMA(implicit val p:NutCoreConfig) extends Module with FMAOpType {
   sfma.io.in.bits.rm := io.in.bits.rm
   sfma.io.in.valid := isSingle(func) && io.in.valid
   sfma.io.ctrl := fma_ctrl
-  sfma.io.out.ready := io.out.fire()
+  sfma.io.out.ready := io.out.ready
   sfma.io.flush := io.flush
 
   val dfma = Module(new FMA_impl(D))
   dfma.io.in.bits := io.in.bits
   dfma.io.in.valid := !isSingle(func) && io.in.valid
   dfma.io.ctrl := fma_ctrl
-  dfma.io.out.ready := io.out.fire()
+  dfma.io.out.ready := io.out.ready
   dfma.io.flush := io.flush
 
-  val s_idle :: s_exec :: s_wait :: Nil = Enum(3)
-  val state = RegInit(s_idle)
+//  val s_idle :: s_exec :: s_wait :: Nil = Enum(3)
+//  val state = RegInit(s_idle)
+//  when(io.flush) {
+//    state := s_idle
+//  }.elsewhen(io.in.fire) {
+//    state := s_exec
+//  }.elsewhen(io.out.fire) {
+//    state := s_idle
+//  }.elsewhen((sfma.io.out.valid || dfma.io.out.valid)) {
+//    state := s_wait
+//  }.elsewhen(state === s_wait && io.out.fire()) {
+//    state := s_idle
+//  } otherwise {
+//    state := state
+//  }
 
-  when(io.flush) {
-    state := s_idle
-  }.elsewhen(io.in.fire) {
-    state := s_exec
-  }.elsewhen(io.out.fire) {
-    state := s_idle
-  }.elsewhen((sfma.io.out.valid || dfma.io.out.valid)) {
-    state := s_wait
-  }.elsewhen(state === s_wait && io.out.fire()) {
-    state := s_idle
-  } otherwise {
-    state := state
-  }
+  val outresult = Mux(isSingle(func), box(sfma.io.out.bits.result, D), dfma.io.out.bits.result)
+  val outfflags = Mux(isSingle(func), sfma.io.out.bits.fflags, dfma.io.out.bits.fflags)
 
-  val outresult = RegNext(Mux(isSingle(func), box(sfma.io.out.bits.result, D), dfma.io.out.bits.result))
-  val outfflags = RegNext(Mux(isSingle(func), sfma.io.out.bits.fflags, dfma.io.out.bits.fflags))
-
-  io.in.ready := state === s_idle
-  io.out.valid := state === s_wait
+  io.in.ready := sfma.io.in.ready & dfma.io.in.ready
+  io.out.valid := sfma.io.out.valid | dfma.io.out.valid
   io.out.bits.result := outresult
   io.out.bits.fflags := outfflags
-  
+
   //if(p.FPGAPlatform){
-    //BoringUtils.addSource(dfma.io.out.valid,"ilaDFMAoutValid")
-    //BoringUtils.addSource(sfma.io.out.valid,"ilaSFMAoutValid")
+  //BoringUtils.addSource(dfma.io.out.valid,"ilaDFMAoutValid")
+  //BoringUtils.addSource(sfma.io.out.valid,"ilaSFMAoutValid")
   //}
 }
-
